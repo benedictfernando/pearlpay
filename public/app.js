9a6f208db8d0f29d72b2c8ac0e1f079bd74140ea
benedictfernando <fernandobenedict619@gmail.com>
2021-10-24 19:28:33 +0800

    appended now built-in forms

--- public/app.js
+++ public/app.js
@@ -1,1 +1,12 @@
-// console.log('hello world!');
+
+$(function() {
+    $('.people-tbl tbody tr').on('click', function() {
+        var tr = $(this); var id = tr.data('id');
+        $.ajax({
+            url: '/contact/' + id,
+            success: function(html) {
+                $('#content').html(html);
+            }
+        });
+    });
+})
