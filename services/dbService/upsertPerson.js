0eb4862de4cbe0a9e45a2c4f45eeb3413edeb141
benedictfernando <fernandobenedict619@gmail.com>
2021-11-04 22:33:48 +0800

    introduced database transactions (begin, commit, rollback)

--- services/dbService/upsertPerson.js
+++ services/dbService/upsertPerson.js
@@ -1,33 +1,58 @@
 
-const db = require('./dbPool');
+const dbPool = require('./dbPool');
 
 module.exports = async ({ id, firstname, lastname, emailaddresses, postaladdresses }) => {
 
-    // convert single string to an array of that single string
-    if (typeof(emailaddresses) === 'string') { emailaddresses = [emailaddresses] }
+    // initialize an empty database
+    let db = null;
 
-    // initialize variable for later manipulation
-    const toInsert = !id; var sql;
-        
-    // insert person for new submissions
-    if(toInsert) {
-        sql = `INSERT INTO people (firstname, lastname, emailaddresses) VALUES ($1, $2, $3)`
-        await db.query(sql, [firstname, lastname, emailaddresses]); return;
-    }
+    try {
+        // start database connection
+        db = await dbPool.connect();
+    
+        // begin transaction
+        await db.query('BEGIN');
 
-    // else, update existing data
-    sql = `UPDATE people SET firstname=$1, lastname=$2, emailaddresses=$4 WHERE id=$3`;
-    await db.query(sql, [firstname, lastname, id, emailaddresses]);
+        // convert single string to an array of that single string
+        if (typeof(emailaddresses) === 'string') { emailaddresses = [emailaddresses] }
 
-    // insert person's postal address(es)
-    postaladdresses.forEach(async pa => {
-        if (!pa.id) {
-            sql = `INSERT INTO postaladdresses (street, city, zipcode) VALUES ($1, $2, $3)`;
-            await db.query(sql, [pa.street, pa.city, pa.zipcode]);
-        } else {
-            // update person's existing postal address(es)
-            sql = `UPDATE postaladdresses SET street=$2, city=$3, zipcode=$4 WHERE id=$1`;
-            await db.query(sql, [pa.id, pa.street, pa.city, pa.zipcode]);
+        // initialize variable for later manipulation
+        const toInsert = !id; var sql;
+            
+        // insert person for new submissions
+        if(toInsert) {
+            sql = `INSERT INTO people (firstname, lastname, emailaddresses) VALUES ($1, $2, $3)`
+            await db.query(sql, [firstname, lastname, emailaddresses]); return;
         }
-    });
+
+        // else, update existing data
+        sql = `UPDATE people SET firstname=$1, lastname=$2, emailaddresses=$4 WHERE id=$3`;
+        await db.query(sql, [firstname, lastname, id, emailaddresses]);
+
+        // insert person's postal address(es)
+        postaladdresses.forEach(async pa => {
+            if (!pa.id) {
+                sql = `INSERT INTO postaladdresses (street, city, zipcode) VALUES ($1, $2, $3)`;
+                await db.query(sql, [pa.street, pa.city, pa.zipcode]);
+            } else {
+                // update person's existing postal address(es)
+                sql = `UPDATE postaladdresses SET street=$2, city=$3, zipcode=$4 WHERE id=$1`;
+                await db.query(sql, [pa.id, pa.street, pa.city, pa.zipcode]);
+            }
+        });
+
+        // push all changes to database if all commits have been successful
+        await db.query('COMMIT');
+    } 
+    catch (err) {
+        // rollback to previous database state otherwise
+        await db.query('ROLLBACK');
+
+        // handle error message...
+        console.log(err);
+    } 
+    finally {
+        // release database connection
+        if (db) { db.release(); }
+    }
 }
