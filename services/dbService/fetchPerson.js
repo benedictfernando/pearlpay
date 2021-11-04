3e0c622de9f3324f476134427349739e49d91c9c
benedictfernando <fernandobenedict619@gmail.com>
2021-11-04 16:34:28 +0800

    created new table w/ data for postal addresses

--- services/dbService/fetchPerson.js
+++ services/dbService/fetchPerson.js
@@ -5,7 +5,17 @@ module.exports = async (id) => {
 
     // create sql query
     const query = await db.query(
-        'SELECT id, firstname, lastname, emailaddresses FROM people WHERE id=$1', [id]
+        `SELECT p.id, p.firstname, p.lastname, p.emailaddresses,
+        json_agg(
+            json_build_object(
+                'id', pa.id,
+                'street', pa.street,
+                'city', pa.city,
+                'zipcode', pa.zipcode
+            )
+        ) postaladdresses
+        FROM people p JOIN postaladdresses pa ON p.id = pa.p_id 
+        WHERE p.id=$1 GROUP BY p.id, p.firstname, p.lastname, p.emailaddresses`, [id]
     );
 
     // returns resulting rows
