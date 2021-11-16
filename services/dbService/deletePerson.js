8ce256bf288338183dc2b5557d4887207e743282
benedictfernando <fernandobenedict619@gmail.com>
2021-11-16 13:59:07 +0800

    update delete queries programmatically

--- services/dbService/deletePerson.js
+++ services/dbService/deletePerson.js
@@ -1,13 +1,30 @@
 
-const db = require('./dbPool');
+const dbPool = require('./dbPool');
 
 module.exports = async (id) => {
 
-    // create sql query
-    const query = await db.query(
-        'DELETE FROM people WHERE id=$1', [id]
-    );
+    try {
+        // start database connection
+        var db = await dbPool.connect();
 
-    // returns results
-    return query;
+        // begin transaction
+        await db.query('BEGIN');
+
+        // delete relative data across tables
+        await db.query('DELETE FROM people WHERE p_id=$1', [id]);
+        await db.query('DELETE FROM postaladdresses WHERE p_id=$1', [id]);
+
+        // commit all if successful
+        await db.query('COMMIT');
+    }
+    
+    catch (err) {
+        // else, print error then rollback
+        console.log(err); await db.query('ROLLBACK');
+    }
+    
+    finally {
+        // release database connection
+        db?.release();
+    }
 }
