641aeba7b54a3fbee549a5cb7ffd997a0d03c6e4
benedictfernando <fernandobenedict619@gmail.com>
2021-11-20 10:43:52 +0800

    configure api's for contacts

---
+++ routes/api/contact.js
@@ -0,0 +1,39 @@
+const { response } = require('express');
+const cryptService = require('../../services/cryptService');
+const fetchPerson = require('../../services/dbService/fetchPerson');
+const upsertPerson = require('../../services/dbService/upsertPerson');
+const deletePerson = require('../../services/dbService/deletePerson');
+const decryptIdMw = require('../../services/middleware/decryptIdMw');
+
+const route = require('express').Router();
+
+route
+
+    // .get('/new', async (request, response) => {
+    //     response.render('person', {
+    //         id: null,
+    //         firstname: null,
+    //         lastname: null,
+    //         emailaddresses: null
+    //     })
+    // })
+
+    .get('/:id', decryptIdMw, async (request, response) => {
+        let { id } = request.params;
+        const data = await fetchPerson(id);
+        response.send(data);
+    })
+
+    .post('/', async (request, response) => {
+        const { body } = request;
+        await upsertPerson(body);
+        response.end();
+    })
+
+    .delete('/:id', async (request, response) => {
+        const { id } = request.params;
+        await deletePerson(id);
+        response.sendStatus(204);
+    });
+
+module.exports = route;
