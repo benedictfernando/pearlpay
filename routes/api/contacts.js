d396a3abf7cff0c2e9cd24b5c51ae84c76e3a638
benedictfernando <fernandobenedict619@gmail.com>
2021-11-23 12:47:22 +0800

    encrypt api's contact person's ids

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,12 +1,14 @@
 const fetchPeople = require('../../services/dbService/fetchPeople');
+const cryptService = require('../../services/cryptService');
 
 const route = require('express').Router();
 
 route
     .get('/', async (request, response) => {
-        
         const contacts = await fetchPeople();
-        response.send({ contacts });
-    })
+        contacts.forEach(contact => {
+            contact.id = cryptService.encrypt(contact.id);
+        }); response.send({ contacts });
+    });
 
 module.exports = route;
